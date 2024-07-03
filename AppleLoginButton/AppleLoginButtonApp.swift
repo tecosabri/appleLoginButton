32ac94abe74e03694c9cbaf36e2d08bada6f4f23
Ismael Sabri <hisme14@gmail.com>
2024-07-03 16:50:46 +0200

    Initial Commit

---
+++ AppleLoginButton/AppleLoginButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleLoginButtonApp.swift
+//  AppleLoginButton
+//
+//  Created by Ismael Sabri Pérez on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleLoginButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
